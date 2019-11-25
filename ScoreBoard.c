9096441ea66d1cf797312e26001dfe86f8374275
rfaturr <54592012+rfaturr@users.noreply.github.com>
2019-11-26 00:53:41 +0700

    Add files via upload

---
+++ ScoreBoard.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+ 
+typedef struct {
+  char name[20];
+  int score;
+}Player;
+
+void addToScoreboard()
+{
+    FILE *scoreboardfile = fopen("scoreboard.csv", "a");
+    Player player;
+    int index,i;
+    
+ 
+    printf("Name: ");
+    scanf("%s", &player.name);
+    printf("Score: ");
+    scanf("%d", &player.score);
+
+    fprintf(scoreboardfile, "%s %d", player.name, player.score);
+    fclose(scoreboardfile);
+ 
+}
+
+void showScoreboard(Player *player)
+{
+    FILE *scoreboardfile = fopen("scoreboard.csv", "r");
+    int i,j,temp;
+    i = 1;
+ 
+    while ((i<100) && (!feof(scoreboardfile)))
+    {
+        fscanf(scoreboardfile, "%s %d", &player->name, &player->score);
+        printf("name: %s, score: %d\n", player->name, player->score);
+        i++;
+    }
+    fclose(scoreboardfile);
+}
+
+
+
+
+int main() {
+  int i, j, temp;
+  Player p;
+
+  showScoreboard(&p);
+  addToScoreboard();
+
+  return 0;
+}
---
+++ ScoreBoard.h
@@ -0,0 +1,18 @@
+#include "boolean.h"
+#include <stdio.h>
+#include <stdio.h>
+ 
+typedef struct {
+  char name[20];
+  int score;
+}Player;
+
+
+void addToScoreboard();
+/* Menerima masukan character dan integer yang akan di cetak
+kedalam file eksternal. file eksternal berupa .csv yang dapat di akses
+di fungsi lain.*/
+
+void showScoreboard(Player *player);
+/* Menampilkan character dan integer dari file eksternal 
+berupa file.csv berurut sesuai urutan dalam file*/

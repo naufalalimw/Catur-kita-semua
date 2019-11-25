4da92c20a697921f6b8e034d8c27d4c7dd37a5ab
rfaturr <54592012+rfaturr@users.noreply.github.com>
2019-11-26 05:38:20 +0700

    Add files via upload

---
+++ driver_mesin_kata.c
@@ -0,0 +1,20 @@
+#include "boolean.h"
+#include "mesin_kata.h"
+#include <stdio.h>
+
+typedef struct {
+	char TabKata[NMax+1];
+	int Length;
+} Kata;
+
+
+int main(){
+    char *filename
+
+    Ignore_Blank();
+    STARTKATA(*filename);
+    ADVKATA();
+    SalinKata();
+
+    return 0;
+}

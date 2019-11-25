ab956de1bea946a559daa6231ef9e346375fb54f
rfaturr <54592012+rfaturr@users.noreply.github.com>
2019-11-26 05:13:44 +0700

    Add files via upload

---
+++ driver_board.c
@@ -0,0 +1,14 @@
+#include "boolean.h"
+#include "board.h"
+#include "listlinier.h"
+#include "queue.h"
+#include "stack.h"
+#include <stdio.h>
+
+int main() {
+    TabChar *T;
+
+    PrintBoard(*T);
+    
+    return 0;
+}

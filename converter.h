4f4285fb1e9e116cb280f95fac5e277f179deb38
Tasya <53542676+peachlight@users.noreply.github.com>
2019-11-25 20:22:04 +0700

    Update converter.h

--- converter.h
+++ converter.h
@@ -4,7 +4,11 @@
 #include "boolean.h"
 
 int CharToInt (char * X);
+/* I.S. Karakter */
+/* F.S. Integer representasi karakter dalam ASCII  */
 
 char IntToChar (int X);
+/* I.S. Integer */
+/* F.S. Karakter yang direpresentasikan integer dalam ASCII */
 
 #endif
